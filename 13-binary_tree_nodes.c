1d4b41cb21652adf3106d532ec52296dd5c41fb3
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-01 10:58:48 -0700

    betty 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_nodes - Counts the node with atlest 1 child
- * @tree: pointer to the root node of the tree to 
+ * @tree: pointer to the root node of the tree to
  * count the number of nodes
  * Return: number of node counted
  **/
@@ -10,28 +10,28 @@
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
-			return (0);
+		return (0);
 
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
-
 /**
- * binary_tree_size - measures the size of binary tree
+ * binary_tree_size - measures the size
+ * of binary tree
  * @tree: pointer to the root node of the tree
  *  to measure the size
  *  Return: size of the tree
- **/
+**/
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
-
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree
+ * binary_tree_leaves - Counts the leaves
+ * in a binary tree
  * @tree: pointer to the root node of the tree to count the
  * number of leaves
  * Return: number of leaves
@@ -39,11 +39,11 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        if (tree->left == NULL && tree->right == NULL)
-                return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-        return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
