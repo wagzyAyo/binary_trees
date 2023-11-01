0c7d073d7fcf954dd24622ff2fb3b1bd53c7ac23
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-01 10:43:14 -0700

    13-main

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,3 +14,36 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	return (binary_tree_size(tree) - binary_tree_leaves(tree));
 }
+
+/**
+ * binary_tree_size - measures the size of binary tree
+ * @tree: pointer to the root node of the tree
+ *  to measure the size
+ *  Return: size of the tree
+ **/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+        if (tree == NULL)
+                return (0);
+
+        return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+
+/**
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree to count the
+ * number of leaves
+ * Return: number of leaves
+ **/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+        if (tree == NULL)
+                return (0);
+
+        if (tree->left == NULL && tree->right == NULL)
+                return (1);
+
+        return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
