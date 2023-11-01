f80be102c3186410b6013a8cdc69884e3551f92b
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-01 04:05:31 -0700

    2-main

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,7 +8,7 @@
  * Return: pointer to the new node or NULL on failure
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
         binary_tree_t *new_node;
 
