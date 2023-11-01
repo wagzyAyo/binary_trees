16a8453656164e1a17faf81428a3f63b4e2d21c2
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-01 05:15:43 -0700

    debug 8-post

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,6 +12,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tre->right, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
